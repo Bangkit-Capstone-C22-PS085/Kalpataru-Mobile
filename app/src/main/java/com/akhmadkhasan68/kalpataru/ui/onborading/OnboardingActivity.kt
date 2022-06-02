b6f6aa1c8a9cbb25aac0ef85063ae160a3aa4d03
Akhmad Khasan Abdullah <41146877+akhmadkhasan68@users.noreply.github.com>
2022-06-02 23:00:44 +0700

    done onboarding

--- app/src/main/java/com/akhmadkhasan68/kalpataru/ui/onborading/OnboardingActivity.kt
+++ app/src/main/java/com/akhmadkhasan68/kalpataru/ui/onborading/OnboardingActivity.kt
@@ -1,7 +1,12 @@
 package com.akhmadkhasan68.kalpataru.ui.onborading
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.akhmadkhasan68.kalpataru.MainActivity
 import com.akhmadkhasan68.kalpataru.R
 import com.akhmadkhasan68.kalpataru.databinding.ActivityOnboardingBinding
 
@@ -13,6 +18,28 @@ class OnboardingActivity : AppCompatActivity() {
         binding = ActivityOnboardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupView()
+        setupActions()
+    }
+
+    private fun setupActions() {
+        binding.btnStart.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
         supportActionBar?.hide()
     }
 }
