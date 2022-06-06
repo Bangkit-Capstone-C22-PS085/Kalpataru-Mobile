5a6dd9e954f6c47cfec70a4923bd471f892bfe4b
iswararesha <reshaiswara222@gmail.com>
2022-06-07 05:02:31 +0700

    Add bottom navigation and fragments. Add Camera implementation

--- app/src/main/java/com/akhmadkhasan68/kalpataru/MainActivity.kt
+++ app/src/main/java/com/akhmadkhasan68/kalpataru/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.akhmadkhasan68.kalpataru
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.akhmadkhasan68.kalpataru.databinding.ActivityMainBinding
+import com.akhmadkhasan68.kalpataru.ui.camera.CameraActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        navView.background = null
+        navView.menu.getItem(2).isEnabled = false
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_sell, R.id.navigation_history, R.id.navigation_profile
+            )
+        )
+
+        binding.scanButton.setOnClickListener(){
+            val intentToCamera = Intent(this@MainActivity, CameraActivity::class.java)
+            startActivity(intentToCamera)
+        }
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/akhmadkhasan68/kalpataru/ui/profile/ProfileFragment.kt
@@ -0,0 +1,40 @@
+package com.akhmadkhasan68.kalpataru.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.akhmadkhasan68.kalpataru.databinding.FragmentProfileBinding
+class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val profileViewModel =
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textProfile
+        profileViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
